12c894c8ca6488aa9b63ab622075fe9bd93fb305
Jhon coneo <jconeo117@gmail.com>
2025-08-12 18:01:55 -0500

    setup service result model

---
+++ Models/ServiceResult.cs
@@ -0,0 +1,15 @@
+﻿namespace DungeonCrawlerAPI.Models
+{
+    public class ServiceResult<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public string? ErrorMessage { get; set; }
+        public List<string> Errors { get; set; } = new();
+
+        public static ServiceResult<T> Success(T result) => new() { IsSuccess = true, Data = result };
+        public static ServiceResult<T> Error(string message) => new() {IsSuccess = false, ErrorMessage = message };
+        public static ServiceResult<T> NotFound(string message) => new() {IsSuccess = false, ErrorMessage = message };
+
+    }
+}
