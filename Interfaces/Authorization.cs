c66ac6e6e3082be370f312001e7476adb96316fd
Jhon coneo <jconeo117@gmail.com>
2025-08-12 18:03:56 -0500

    partially setup Auth repository & service

---
+++ Interfaces/Authorization.cs
@@ -0,0 +1,31 @@
+﻿using DungeonCrawlerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DungeonCrawlerAPI.Interfaces
+{
+
+    public interface IAuthorization : IRepository<MUser>
+    {
+        Task<MUser?> GetByEmailAsync(string email);
+        Task<MUser?> GetByUsernameAsync(string username);
+
+    }
+
+
+    public class Authorization : Repository<MUser>, IAuthorization
+    {
+        public Authorization(AppDBContext context) : base(context)
+        {
+        }
+
+        public async Task<MUser?> GetByEmailAsync(string email)
+        {
+            return await _dbset.FirstOrDefaultAsync(us =>  us.Email == email);
+        }
+
+        public async Task<MUser?> GetByUsernameAsync(string username)
+        {
+            return await _dbset.FirstOrDefaultAsync(us =>  us.Username == username);
+        }
+    }
+}
