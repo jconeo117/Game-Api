eb5324561f3b676c66e05525e07c0b90154f8efd
Jhon coneo <jconeo117@gmail.com>
2025-08-13 00:50:08 -0500

    Setup Data Tranfers Objects

---
+++ DTO's/AccountDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace DungeonCrawlerAPI.DTO_s
+{
+    public class RegisterDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RemenberMe { get; set; }
+    }
+
+    public class ChangePasswordDTO
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
---
+++ DTO's/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DungeonCrawlerAPI.DTO_s
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
