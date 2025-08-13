e9e8ca1dce5d20bceec8cd9680b500ac54aa7413
Jhon coneo <jconeo117@gmail.com>
2025-08-13 00:51:55 -0500

    setup Auth service interface

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,5 +1,24 @@
-﻿namespace DungeonCrawlerAPI.Services
+﻿using DungeonCrawlerAPI.DTO_s;
+using DungeonCrawlerAPI.Models;
+
+namespace DungeonCrawlerAPI.Services
 {
+
+    public interface IAuthService
+    {
+        Task<ServiceResult<UserDTO>> RegisterAsync(RegisterDTO user);
+        Task<ServiceResult<UserDTO>> LoginAsync(LoginDTO user);
+        Task<ServiceResult<UserDTO>> UpdateProfileAsync(string id, UserDTO user);
+        Task<ServiceResult<bool>> DeleteProfileAsync(string id);
+
+        Task<ServiceResult<bool>> ChangePasswordAsync(string id, ChangePasswordDTO changePasswordDTO);
+        Task<ServiceResult<UserDTO>> GetProfileAsync(string id);
+
+        Task<ServiceResult<bool>> ValidateUsernameExistsAsync(string username);
+        Task<ServiceResult<bool>> ValidateEmailExistsAsync(string Email);
+    }
+
+
     public class AuthService
     {
     }
