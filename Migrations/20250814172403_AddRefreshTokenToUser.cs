d520daeb3918a26a902e956a02ad6f612b630db0
Jhon coneo <jconeo117@gmail.com>
2025-08-14 12:29:56 -0500

    db update migration

---
+++ Migrations/20250814172403_AddRefreshTokenToUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DungeonCrawlerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Expires_In",
+                table: "Players",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Refresh_Token",
+                table: "Players",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Expires_In",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Refresh_Token",
+                table: "Players");
+        }
+    }
+}
