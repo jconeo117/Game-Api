9a51dd00343c32e3bb4de452a2da065680c27cae
Jhon coneo <jconeo117@gmail.com>
2025-08-22 19:00:20 -0500

    Setup character controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,6 +1,61 @@
-﻿namespace DungeonCrawlerAPI.Controllers
+﻿using DungeonCrawlerAPI.DTO_s;
+using DungeonCrawlerAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace DungeonCrawlerAPI.Controllers
 {
-    public class CharacterController
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CharacterController : ControllerBase
     {
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateCharacter([FromBody] CreateCharacterDTO characterDTO)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? User.FindFirstValue("sub");
+
+            if(userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var response = await _characterService.CreateCharacterAsync(userId, characterDTO);
+            if (!response.IsSuccess)
+            {
+                return BadRequest(response.ErrorMessage);
+            }
+
+            return Ok(response.Data);
+        }
+
+        [HttpGet("my-character")]
+        public async Task<IActionResult> GetCharacter()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? User.FindFirstValue("sub");
+
+            if(userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var response = await _characterService.GetCharacterByUserId(userId);
+            if (!response.IsSuccess)
+            {
+                return NotFound(response.ErrorMessage);
+            }
+
+            return Ok(response.Data);
+        }
+
     }
 }
