44f199b20f19e099586d28cf1bfb558f78a18244
Jhon coneo <jconeo117@gmail.com>
2025-09-03 00:24:25 -0500

    create Equipment slots respository

---
+++ Interfaces/EquipmentSlotRepository.cs
@@ -0,0 +1,25 @@
+﻿using DungeonCrawlerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DungeonCrawlerAPI.Interfaces
+{
+
+    public interface IEquipmentSlotRepository : IRepository<MEquipmentSlot>
+    {
+        Task<MEquipmentSlot?> GetSlotByCharacterAndTypeAsync(string characterId, EquipmentSlotType slotType);
+
+    }
+
+    public class EquipmentSlotRepository : Repository<MEquipmentSlot>, IEquipmentSlotRepository
+    {
+        public EquipmentSlotRepository(AppDBContext context) : base(context)
+        {
+        }
+
+        public async Task<MEquipmentSlot?> GetSlotByCharacterAndTypeAsync(string characterId, EquipmentSlotType slotType)
+        {
+            return await _dbset.FirstOrDefaultAsync(s => s.CharacterId == characterId && s.SlotType == slotType);
+
+        }
+    }
+}
