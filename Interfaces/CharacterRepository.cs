ecdad3c19c8939f6a2214c06898bfb2a5e17a996
Jhon coneo <jconeo117@gmail.com>
2025-09-03 19:08:33 -0500

    add get all profile character method to repository

--- Interfaces/CharacterRepository.cs
+++ Interfaces/CharacterRepository.cs
@@ -8,6 +8,7 @@ namespace DungeonCrawlerAPI.Interfaces
     {
         Task<MCharacter?> GetCharByUserIdAsync(string UserId);
         Task<MCharacter?> GetByNameAsync(string Name, string UserId);
+        Task<MCharacter?> GetCharacterProfileByIdAsync(string CharacterId);
     }
 
     public class CharacterRepository : Repository<MCharacter>, ICharacterRepository
@@ -19,6 +20,11 @@ namespace DungeonCrawlerAPI.Interfaces
         public async Task<MCharacter?> GetByNameAsync(string Name, string UserId)
         {
             return await _dbset.FirstOrDefaultAsync(c => c.Name == Name && c.UserId == UserId);
+        }
+
+        public async Task<MCharacter?> GetCharacterProfileByIdAsync(string CharacterId)
+        {
+            return await _dbset.Include(c => c.EquipmentSlots).ThenInclude(es => es.Item).FirstOrDefaultAsync(c => c.Id == CharacterId);
         }
 
         public async Task<MCharacter?> GetCharByUserIdAsync(string UserId)
