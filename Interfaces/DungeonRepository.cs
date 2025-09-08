1a857291200713b16b27fc27de007288982d7194
Jhon coneo <jconeo117@gmail.com>
2025-09-08 18:10:51 -0500

    setup dungeon repository

---
+++ Interfaces/DungeonRepository.cs
@@ -0,0 +1,34 @@
+﻿using DungeonCrawlerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DungeonCrawlerAPI.Interfaces
+{
+    public interface IDungeonRepository : IRepository<MDungeon>
+    {
+
+    }
+
+    public class DungeonRepository : Repository<MDungeon>, IDungeonRepository
+    {
+        public DungeonRepository(AppDBContext context) : base(context)
+        {
+        }
+    }
+
+    public interface IDungeonRunRepository: IRepository<MDungeonRun>
+    {
+        Task<IEnumerable<MDungeonRun>> GetRunsByCharacterId(string characterId); 
+    }
+
+    public class DungeonRunRepository : Repository<MDungeonRun>, IDungeonRunRepository
+    {
+        public DungeonRunRepository(AppDBContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<MDungeonRun>> GetRunsByCharacterId(string characterId)
+        {
+            return await _dbset.Where(dr => dr.CharacterId == characterId).ToListAsync();
+        }
+    }
+}
