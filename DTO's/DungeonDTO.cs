055b40d1464947e3bca54be1bf90248ba8175351
Jhon coneo <jconeo117@gmail.com>
2025-09-08 18:10:26 -0500

    setup dungeons crud data transfer object

---
+++ DTO's/DungeonDTO.cs
@@ -0,0 +1,39 @@
+﻿namespace DungeonCrawlerAPI.DTO_s
+{
+    public class DungeonDTO
+    {
+        public string id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int difficulty { get; set; }
+    }
+
+    public class CreateDungeonDTO
+    {
+        public string name { get; set;}
+        public string description { get; set;}
+        public int difficulty { get; set; }
+    }
+
+    public class UpdateDungeonDTO
+    {
+        public string? name { get; set;}
+        public string? description { get; set;}
+        public int? difficulty { get; set; }
+    }
+
+    public class DungeonRunDTO
+    {
+        public string id { get; set;}
+        public string DungeonName { get; set;}
+        public bool IsSuccess { get; set;}
+        public int CompletionTime { get; set;}
+        public DateTime CreatedAt { get; set;}
+    }
+
+    public class CompleteDungeonRunDTO
+    {
+        public bool IsSuccess { get; set;}
+        public int CompletionTime { get; set;}
+    }
+}
