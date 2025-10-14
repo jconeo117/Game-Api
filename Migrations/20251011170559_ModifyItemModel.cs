b38b104bef11411cc03e1eef4f2ef44b55645a7e
Jhon coneo <jconeo117@gmail.com>
2025-10-14 17:29:20 -0500

    migration Db: modify item model

---
+++ Migrations/20251011170559_ModifyItemModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DungeonCrawlerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyItemModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Is_System_Item",
+                table: "Items",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Is_System_Item",
+                table: "Items");
+        }
+    }
+}
